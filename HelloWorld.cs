8f6ce217b2cd5192d8fa1f8f231fb0899ee8ea9b
Salman Faris <Salman Faris>
2016-12-18 11:35:32 +0530

    new

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld_MES_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(args[0]);
+            
+        }
+    }
+}
